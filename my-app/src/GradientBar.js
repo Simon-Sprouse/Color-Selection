b1e6b28436de95e8f02400ca00810bfcac2cb724
Simon Sprouse <simonhsprouse@gmail.com>
2024-11-11 17:27:34 -0600

    fader for gradient bar

--- my-app/src/GradientBar.js
+++ my-app/src/GradientBar.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 
 import { hsvToRgbString } from './colorFunctions';
 
@@ -7,18 +7,27 @@ function GradientBar({hsvValues}) {
     const canvasRef = useRef(null);
     const barHeight = 50;
 
+    const [dotPosition, setDotPosition] = useState({x: 300, y:50});
+    const [isDragging, setIsDragging] = useState(false);
+
+    const [dots, setDots] = useState([]);
+
+
     useEffect(() => { 
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         const gradient = ctx.createLinearGradient(0, barHeight, canvas.width, barHeight);
+
+
 
         if (hsvValues.length >= 2) { 
 
 
-
-
-            gradient.addColorStop(0, hsvObjectToRgbString(hsvValues[0]));
+            const distanceAlongGradient = Math.max(0, dotPosition.x / canvas.width);
+           
+            gradient.addColorStop(distanceAlongGradient, hsvObjectToRgbString(hsvValues[0]));
             gradient.addColorStop(1, hsvObjectToRgbString(hsvValues[1]));
         }
         else { 
@@ -31,12 +40,58 @@ function GradientBar({hsvValues}) {
         ctx.fillRect(0, 0, canvas.width, barHeight);
 
 
-    }, [hsvValues]);
+
+
+        // draw the dot
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        ctx.arc(dotPosition.x, dotPosition.y, 33, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.fillStyle = hsvObjectToRgbString(hsvValues[0]);
+        ctx.beginPath();
+        ctx.arc(dotPosition.x, dotPosition.y, 30, 0, Math.PI * 2);
+        ctx.fill();
+
+
+
+    }, [hsvValues, dotPosition]);
 
 
     function hsvObjectToRgbString(hsv) { 
         return hsvToRgbString(hsv.h, hsv.s, hsv.v)
     }
+
+
+
+
+
+    function handleMouseDown(event) { 
+        const canvas = canvasRef.current;
+        const rect = canvas.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+
+        setDotPosition({x: mouseX, y:mouseY});
+        setIsDragging(true);
+
+    }
+
+    function handleMouseMove(event) { 
+        if (!isDragging) return;
+
+        const canvas = canvasRef.current;
+        const rect = canvas.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+
+        setDotPosition({x: mouseX, y:mouseY});
+    }
+
+    function handleMouseUp() { 
+        setIsDragging(false);
+    }
+
 
 
 
@@ -47,7 +102,15 @@ function GradientBar({hsvValues}) {
                 <p key={index}>{JSON.stringify(value)}</p>
             ))} */}
 
-            <canvas ref={canvasRef} height="100px" width="1200px"></canvas>
+            <canvas 
+                ref={canvasRef} 
+                height="100px" 
+                width="1200px"
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+                onMouseLeave={handleMouseUp}
+            ></canvas>
         </>
     )
 }
