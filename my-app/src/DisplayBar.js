533c5dfea92682b6e7ee667455953cf673d65288
Simon Sprouse <simonhsprouse@gmail.com>
2024-11-12 23:53:14 -0600

    discrete and continuous display

--- my-app/src/DisplayBar.js
+++ my-app/src/DisplayBar.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 import { hsvToRgbString } from './colorFunctions';
 
@@ -6,6 +6,9 @@ function DisplayBar({hsvValues, positions}) {
 
 
     const canvasRef = useRef(null);
+    const [numPanels, setNumPanels] = useState(2);
+
+    const [style, setStyle] = useState(0); // 0 continuous, 1 discrete
 
 
 
@@ -16,15 +19,14 @@ function DisplayBar({hsvValues, positions}) {
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
+        
 
 
-        const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-
-
-
+       // CONTINUOUS GRADIENT BAR // 
 
         if (hsvValues.length >= 2 && hsvValues.length == positions.length) { 
 
+            const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
 
             for (let i = 0; i < hsvValues.length; i++) { 
 
@@ -35,12 +37,77 @@ function DisplayBar({hsvValues, positions}) {
 
         }
 
+        // DISCRETE GRADIENT BAR // 
 
-    }, [hsvValues, positions]);
+        // console.log("Discrete Bar");
+
+        if (style == 0) {
+            if (hsvValues.length >= 2 && hsvValues.length == positions.length) { 
+
+                // const numPanels = 5;
+
+                const discreteGradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+
+                for (let i = 0; i < numPanels; i++) { 
+
+
+
+
+
+                    const lowDistance = Math.round((i / numPanels) * 100) / 100;
+                    const highDistance = Math.round(((i+1) / numPanels) * 100) / 100;
+
+                    const medDistance = (lowDistance + highDistance) / 2;
+                    const pixelColor = getPixelColor(medDistance);
+
+
+                    discreteGradient.addColorStop(lowDistance, pixelColor);
+                    discreteGradient.addColorStop(highDistance, pixelColor);
+                
+                }
+
+                ctx.fillStyle = discreteGradient;
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            }
+
+        }
+        
+
+        
+
+
+
+
+
+
+    }, [hsvValues, positions, numPanels, style]);
 
     function hsvObjectToRgbString(hsv) { 
         return hsvToRgbString(hsv.h, hsv.s, hsv.v)
     }
+
+
+    function getPixelColor(distance) { 
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+
+        // distance is between 0 and 1
+
+        if (distance > 0.99) { 
+            distance = 0.99;
+        }
+
+        const xPos = canvas.width * distance;
+
+        const pixelData = ctx.getImageData(xPos, 0, 1, 1).data; // r, g, b, a
+        const [r, g, b, a] = pixelData;
+        return `rgb(${r}, ${g}, ${b})`
+    }
+
+
+
 
 
 
@@ -51,6 +118,10 @@ function DisplayBar({hsvValues, positions}) {
                 <p key={index}>Position: {pos}  Color: {JSON.stringify(hsvValues[index])}</p>
             ))}
             <p>Purple hair, purple hair, I'm a fuzzy fertile bear</p> */}
+        
+            <button onClick={() => setStyle(prevStyle => prevStyle == 1 ? 0 : 1)}>Style</button>
+            <input type="range" min="1" max="20" value={numPanels} onChange={(e) => setNumPanels(e.target.value)}/>
+            <p></p>
             <canvas width="1200" height="200" ref={canvasRef}></canvas>
         </>
     )
